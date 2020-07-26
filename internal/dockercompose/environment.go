984fe43463f0e3fddd0416bc317dc7ba7fc36aca
Artem Trushkin <at@kodix.ru>
2020-07-26 16:36:13 +0300

    Create dockercompose environment

---
+++ internal/dockercompose/environment.go
@@ -0,0 +1,40 @@
+package dockercompose
+
+import (
+	"fmt"
+	"strings"
+)
+
+type Environment map[string]string
+
+func (e Environment) String() string {
+	length := len(e)
+
+	if length == 0 {
+		return ""
+	}
+
+	var sb strings.Builder
+	sb.WriteString("environment:\n")
+
+	i := 1
+	for variable, value := range e {
+		if variable == "" {
+			continue
+		}
+
+		if value == "" {
+			sb.WriteString(fmt.Sprintf("  %s:", variable))
+		} else {
+			sb.WriteString(fmt.Sprintf("  %s: %s", variable, value))
+		}
+
+		if i != length {
+			sb.WriteString("\n")
+		}
+
+		i++
+	}
+
+	return sb.String()
+}
---
+++ internal/dockercompose/environment_test.go
@@ -0,0 +1,38 @@
+package dockercompose_test
+
+import (
+	"testing"
+
+	"github.com/Bocmah/phpdocker-scaffold/internal/dockercompose"
+)
+
+func TestEnvironment_String(t *testing.T) {
+	tests := map[string]struct {
+		input dockercompose.Environment
+		want  string
+	}{
+		"simple": {
+			input: dockercompose.Environment{
+				"SOME_VAR": "foo",
+			},
+			want: `environment:
+  SOME_VAR: foo`,
+		},
+		"with empty value": {
+			input: dockercompose.Environment{
+				"SOME_VAR": "",
+			},
+			want: `environment:
+  SOME_VAR:`,
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := tc.input.String()
+			if tc.want != got {
+				t.Fatalf("expected:\n %v\n got:\n %v", tc.want, got)
+			}
+		})
+	}
+}
