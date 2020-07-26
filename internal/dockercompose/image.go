7fa708ee26b2ee842f250ca5e39ead50d2f193cf
Artem Trushkin <at@kodix.ru>
2020-07-26 16:58:49 +0300

    Implement Stringer interface in image

---
+++ internal/dockercompose/image.go
@@ -0,0 +1,26 @@
+package dockercompose
+
+import "strings"
+
+type Image struct {
+	Name string
+	Tag  string
+}
+
+func (i Image) String() string {
+	if i.Name == "" {
+		return ""
+	}
+
+	var sb strings.Builder
+	sb.WriteString("image: ")
+
+	if i.Tag == "" {
+		sb.WriteString(i.Name)
+		return sb.String()
+	}
+
+	sb.WriteString(Mapping(i.Name, i.Tag))
+
+	return sb.String()
+}
---
+++ internal/dockercompose/image_test.go
@@ -0,0 +1,36 @@
+package dockercompose_test
+
+import (
+	"testing"
+
+	"github.com/Bocmah/phpdocker-scaffold/internal/dockercompose"
+)
+
+func TestImage_String(t *testing.T) {
+	tests := map[string]struct {
+		input dockercompose.Image
+		want  string
+	}{
+		"simple": {
+			input: dockercompose.Image{Name: "nginx", Tag: "alpine"},
+			want:  "image: nginx:alpine",
+		},
+		"no tag": {
+			input: dockercompose.Image{Name: "nginx"},
+			want:  "image: nginx",
+		},
+		"no tag and no name": {
+			input: dockercompose.Image{},
+			want:  "",
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := tc.input.String()
+			if tc.want != got {
+				t.Fatalf("expected: %v, got: %v", tc.want, got)
+			}
+		})
+	}
+}
