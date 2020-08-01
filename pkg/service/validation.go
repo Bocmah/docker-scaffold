b84f630fc6d4f3e44aa347cac6320b13258c2ad7
Artem Trushkin <at@kodix.ru>
2020-08-01 12:28:17 +0300

    Change semantics in ValidationErrors

--- pkg/service/validation.go
+++ pkg/service/validation.go
@@ -4,20 +4,20 @@ import "strings"
 
 type ValidationErrors []string
 
-func (v *ValidationErrors) Error() string {
-	return strings.Join(*v, "\n")
+func (v ValidationErrors) Error() string {
+	return strings.Join(v, "\n")
 }
 
 func (v *ValidationErrors) Add(err ...string) {
 	*v = append(*v, err...)
 }
 
-func (v *ValidationErrors) IsEmpty() bool {
-	return len(*v) == 0
+func (v ValidationErrors) IsEmpty() bool {
+	return len(v) == 0
 }
 
-func (v *ValidationErrors) Has(err string) bool {
-	for _, el := range *v {
+func (v ValidationErrors) Has(err string) bool {
+	for _, el := range v {
 		if el == err {
 			return true
 		}
